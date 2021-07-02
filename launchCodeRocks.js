992dce604021741ca26a1bcfffbd5a8ed9cee9ed
Ramadevi Maram <deviduggireddy@gmail.com>
2021-07-02 20:44:34 +0000

    DRY code

--- launchCodeRocks.js
+++ launchCodeRocks.js
@@ -1,22 +1,53 @@
+// function launchOutput(num){
+//   //your code here
+//   let output='';
+//   if (num%2 === 0){
+//       output += 'Launch';
+//     }
+  
+//     if (num%3 === 0){
+//       output += 'Code';
+//     }
+  
+//     if (num%5 === 0){
+//       if (output) {
+//         output += ' Rocks';
+//       }else{
+//         output += 'Rocks';
+//         }
+//     }
+  
+//     if (!output){
+//       return "Rutabagas! That doesn't work.";
+//     } else if (output==='Launch Rocks') {
+//       return output + '! (CRASH!!!!)';
+//     } else {
+//       return output + '!';
+//     }
+
+// }
+
+//DRY Code
 function launchOutput(num){
   //your code here
-  let output='';
-  if (num%2 === 0){
-      output += 'Launch';
-    }
-  
-    if (num%3 === 0){
-      output += 'Code';
-    }
-  
-    if (num%5 === 0){
-      if (output) {
-        output += ' Rocks';
-      }else{
-        output += 'Rocks';
-        }
-    }
-  
+   let output = '';
+   let numberObject ={
+     '2': 'Launch',
+     '3': 'Code',
+     '5': 'Rocks'
+   }
+
+   for(item in numberObject){
+     if(num%Number(item) === 0){
+       if(Number(item) === 5 && output){
+         output += ` ${numberObject[item]}`
+       }
+       else {
+         output += numberObject[item]
+       }
+     }
+   }
+    
     if (!output){
       return "Rutabagas! That doesn't work.";
     } else if (output==='Launch Rocks') {
